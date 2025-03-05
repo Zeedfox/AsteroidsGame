30ec4bfefba239999e3c5419047f8ae0de5e0046
Antonio Bal <tonylodolfo@hotmail.com>
2025-03-05 00:07:24 -0600

    Se realizo la aparicion del jugador y se añadio la posibilidad de rotar hacia la izquierda y derecha

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     #This initialize the module of pygame. The library for games.
     pygame.init() 
     #We will call a function to set up the window/display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_ship = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2),PLAYER_RADIUS)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -20,13 +22,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        print("Frame???")
         #Painting the screen with black
         pygame.Surface.fill(screen, (0,0,0))
+        #funtion of roting the player
+        player_ship.update(dt)
+        #Print the player on the screen
+        player_ship.draw(screen)
+
         #Refreshing the Screen: it is important to be on the end
+        #player_ship.rotation += 1 #let's rotate xD
         pygame.display.flip()
         dt = (clock.tick(60)/1000) #this will pause the game loop
 
+    #JUST FOR TESTING!!!!!!!! FIRST ATTEMPTS
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
